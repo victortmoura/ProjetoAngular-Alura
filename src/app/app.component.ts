c2ac5d4d335b04386bb3896bbbf755f82faeb151
Victor Moura <jose.moura@hivecloud.com.br>
2019-09-05 19:41:24 -0300

    iniciando o projeto

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = "Victor";
+  url = "https://i.pinimg.com/originals/5b/be/51/5bbe514dd733c24cf53b26dcc5b61d30.jpg";
+  description="leão";
+}
